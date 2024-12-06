481f222f2f471d409dbee01dc3e36fbed267a582
rhmnx <116880430+rhmnx@users.noreply.github.com>
2024-12-06 21:17:47 +0700

    Add marker to map

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import psycopg2
+import geojson
 
 app = Flask(__name__)
+
+# Koneksi ke database PostgreSQL
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname="sig", 
+        user="postgres", 
+        password="Roman5432", 
+        host="localhost", 
+        port="5432"
+    )
+    return conn
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get_geojson')
+def get_geojson():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # Ambil data objek wisata dalam format GeoJSON
+    cursor.execute("""
+        SELECT gid, nama_objek, jenis_obje, alamat, deskripsi, ST_AsGeoJSON(geom) AS geojson
+        FROM "pariwisata lampung";
+    """)
+    
+    rows = cursor.fetchall()
+    
+    # Membentuk GeoJSON
+    features = []
+    for row in rows:
+        gid, nama_objek, jenis_obje, alamat, deskripsi, geojson_str = row
+        feature = {
+            "type": "Feature",
+            "properties": {
+                "gid": gid,
+                "nama_objek": nama_objek,
+                "jenis_obje": jenis_obje,
+                "alamat": alamat,
+                "deskripsi": deskripsi
+            },
+            "geometry": geojson.loads(geojson_str)
+        }
+        features.append(feature)
+    
+    geojson_data = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    cursor.close()
+    conn.close()
+    
+    return jsonify(geojson_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/map.js
+++ static/js/map.js
@@ -2,23 +2,23 @@
 const map = L.map('map').setView([-5.4295, 105.2610], 13); // Lokasi Bandar Lampung
 
 // Tambahkan peta dasar dari Mapbox
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=AccessToken', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=Token', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'AccessToken' // Ganti dengan token Mapbox Anda
+    accessToken: 'Token' // Ganti dengan token Mapbox Anda
 }).addTo(map);
 
-// Tambahkan batas wilayah (contoh GeoJSON)
-const geojsonLayer = L.geoJSON(yourGeoJsonData, {
-    style: {
-        color: "#FF6600",
-        weight: 2
-    }
-}).addTo(map);
-
-// Tambahkan marker kategori
-L.marker([-5.4295, 105.2610]).addTo(map)
-    .bindPopup('Bandar Lampung')
-    .openPopup();
+// Fetch data GeoJSON dari server Flask
+fetch('/data')
+    .then(response => response.json())
+    .then(data => {
+        // Tambahkan layer GeoJSON ke peta
+        L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(`<b>Nama Objek:</b> ${feature.properties.nama_objek}<br><b>Jenis Objek:</b> ${feature.properties.jenis_obje}`);
+            }
+        }).addTo(map);
+    })
+    .catch(error => console.error('Error loading GeoJSON:', error));
