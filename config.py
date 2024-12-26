7dd5570c858d94384cd4a278df9183a4e94be304
IT131313 <96949378+IT131313@users.noreply.github.com>
2024-12-26 18:46:25 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 DATABASE_CONFIG = {
     'dbname': 'pariwisata_db',   # Nama database
     'user': 'postgres',         # Nama pengguna database
-    'password': 'BangKumis69',  # Kata sandi pengguna
-    'host': 'localhost',         # Host database
-    'port': '6699',              # Port database
+    'password': 'postgres123',  # Kata sandi pengguna
+    'host': '34.101.173.163',         # Host database
+    'port': '5432',              # Port database
 }
